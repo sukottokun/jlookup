669a9b186d67422918bcb47487f3f689124cefba
Scott Massey <scott.massey@gmail.com>
2013-12-28 23:02:43 -0800

    back to creating classes

--- kanji.py
+++ kanji.py
@@ -5,28 +5,34 @@ __author__ = 'scott'
 
 class Kanji:
     def __init__(self, ji, meaning=''):
-        self.ji = ji
-        self.meaning = meaning
-        self.seen = 0
-        self.known = 0
+        self._ji = ji
+        self._meaning = meaning
+        self._seen = 0
+        self._known = 0
+        print "Kanji object is created."
 
-    def set_meaning(self, meaning):
-        self.meaning = meaning
 
     def increment_seen(self, amount):
-        self.seen += amount
+        self._seen += amount
 
     def increment_seen_by_one(self):
         self.increment_seen(1)
 
+    def __repr__(self):
+        return self._ji
+
+    def __str__(self):
+        return "An instance of class Test with state: a=%s" % self._ji
+
 
 
 if __name__ == '__main__':
-    a = Kanji('私')
-    a.set_meaning('I')
+    a = Kanji('猫')
+    a.meaning = "I, Myself"
     a.increment_seen(2)
     a.increment_seen_by_one()
-    print a.meaning
-    print a.seen
-    print "a means %s" % a.meaning
+    print a._meaning
+    print a._seen
+    print "a means %s" % a._meaning
+    print a
 
