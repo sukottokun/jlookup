1e60477857f15cbd928ff9f4dec0ea53dbcc508c
Scott Massey <scott.massey@gmail.com>
2013-12-29 14:04:01 -0800

    maybe done with classes

--- kanji.py
+++ kanji.py
@@ -5,24 +5,24 @@ __author__ = 'scott'
 
 class Kanji:
     def __init__(self, ji, meaning=''):
-        self._ji = ji
-        self._meaning = meaning
-        self._seen = 0
-        self._known = 0
+        self.ji = ji
+        self.meaning = meaning
+        self.seen = 0
+        self.known = 0
         print "Kanji object is created."
 
 
     def increment_seen(self, amount):
-        self._seen += amount
+        self.seen += amount
 
     def increment_seen_by_one(self):
         self.increment_seen(1)
 
     def __repr__(self):
-        return self._ji
+        return self.ji
 
     def __str__(self):
-        return "An instance of class Test with state: a=%s" % self._ji
+        return self.ji
 
 
 
@@ -31,8 +31,5 @@ if __name__ == '__main__':
     a.meaning = "I, Myself"
     a.increment_seen(2)
     a.increment_seen_by_one()
-    print a._meaning
-    print a._seen
-    print "a means %s" % a._meaning
-    print a
+    print("{0} means {1}, and has been seen {2} times".format(a, a.meaning, a.seen))
 
